506c729be9e6aabdd4fb5bcea76ab58f1ca5ac96
sebastiangancino <sjgancino97sd@gmail.com>
2022-03-29 23:58:14 -0500

    Regreso hasta todo bien de la clase 21

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,8 +3,13 @@ import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg';
 import AppContext from '../context/AppContext';
 
+<<<<<<< HEAD
 const ProductItem = ({product}) => {
 	const {addToCart} = useContext(AppContext);
+=======
+const ProductItem = () => {
+	const [cart, setCart] = useState([]);
+>>>>>>> parent of a7c2409... Todo bien hasta clase 21
 
 	const handleClick = item => {
 		addToCart([item]);
@@ -12,11 +17,11 @@ const ProductItem = ({product}) => {
 
 	return (
 		<div className="ProductItem">
-			<img src={product.images[0]} alt={product.title} />
+			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
 			<div className="product-info">
 				<div>
-					<p>${product.price}</p>
-					<p>{product.title}</p>
+					<p>$120,00</p>
+					<p>Bike</p>
 				</div>
 				<figure onClick={handleClick(product)} >
 					<img src={addToCart} alt="" />
